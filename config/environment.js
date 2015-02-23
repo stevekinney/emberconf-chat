f573aa291b8c3d25827399cf67b3421e8a36aa7c
Steve Kinney <hello@stevekinney.net>
2015-02-22 21:34:24 -0700

    Update security policy in development

--- config/environment.js
+++ config/environment.js
@@ -20,7 +20,9 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
+    ENV.contentSecurityPolicy = {
+      'connect-src': "'self' ws://localhost:8080",
+    };
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
