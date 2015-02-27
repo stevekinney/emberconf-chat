20380bb4402f3d03b2164a8b19e99005c94a297a
Robert Jackson <robert.w.jackson@me.com>
2015-02-27 16:17:15 -0500

    Remove unused initializer.
    It doesn't seem that any routes use the `websocketService`.

--- app/initializers/websocket-service.js
+++
@@ -1,8 +0,0 @@
-export function initialize(container, application) {
-  application.inject('route', 'websocketService', 'service:websocket');
-}
-
-export default {
-  name: 'websocket-service',
-  initialize: initialize
-};
